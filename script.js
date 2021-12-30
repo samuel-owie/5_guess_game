bb3c88f520aae643deda4537985c90f8496f4062
samuel-owie <owiesamuelehizogie@gmail.com>
2021-12-30 04:51:27 +0100

    completion of the guess game

--- script.js
+++ script.js
@@ -93,5 +93,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').style.width = '14rem';
 });
